ad776684832179322327098b306ce4b93e8e3157
Axel Etcheverry <axel@etcheverry.biz>
2021-08-03 01:18:02 +0200

    feat(example): add first exemple of OAuth2

--- example/oauth2/go.mod
+++ example/oauth2/go.mod
@@ -2,6 +2,10 @@ module github.com/hyperscale-stack/security/example/oauth2
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gilcrest/alice v1.0.0
+	github.com/gorilla/mux v1.8.0
+	github.com/hyperscale-stack/security v0.0.0-20210721230237-494160d3eb0e
+)
 
-replace github.com/hyperscale-stack/security/ => ../../
+replace github.com/hyperscale-stack/security => ../../
--- example/oauth2/main.go
+++ example/oauth2/main.go
@@ -1,12 +1,19 @@
+// Copyright 2021 Hyperscale. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
 	"net/http"
 
+	"github.com/gilcrest/alice"
 	"github.com/gorilla/mux"
 	"github.com/hyperscale-stack/security/authentication"
 	"github.com/hyperscale-stack/security/authentication/provider/oauth2"
+	"github.com/hyperscale-stack/security/authentication/provider/oauth2/storage"
 	"github.com/hyperscale-stack/security/authentication/provider/oauth2/token/random"
+	"github.com/hyperscale-stack/security/authorization"
 )
 
 func main() {
@@ -14,7 +21,13 @@ func main() {
 
 	tokenGenerator := random.NewTokenGenerator(&random.Configuration{})
 
-	storageProvider := NewOAuth2Storage()
+	storageProvider := storage.NewInMemoryStorage()
+
+	storageProvider.SaveClient(&oauth2.DefaultClient{
+		ID:          "5cc06c3b-5755-4229-958c-a515a245aaeb",
+		Secret:      "WTvuAztPD2XBauomleRzGFYuZawS07Ym",
+		RedirectURI: "https://connect.myservice.tld",
+	})
 
 	// Add authentication filters
 	r.Use(authentication.FilterHandler(
@@ -28,7 +41,17 @@ func main() {
 		oauth2.NewOAuth2AuthenticationProvider(tokenGenerator, storageProvider),
 	))
 
-	r.
+	private := alice.New(
+		authorization.AuthorizeHandler(),
+	)
+
+	r.Handle("/protected", private.ThenFunc(func(w http.ResponseWriter, r *http.Request) {
+		// private route
+	})).Methods(http.MethodGet)
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// public route
+	}).Methods(http.MethodGet)
 
 	if err := http.ListenAndServe(":1337", r); err != nil {
 		panic(err)
